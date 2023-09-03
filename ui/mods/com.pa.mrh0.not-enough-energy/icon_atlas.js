24c010174c5e862a097da05b435bd9bafdf5ec4e
MRH0 <mrhmail1@gmail.com>
2023-09-03 13:12:06 +0200

    added legion energy titan

--- ui/mods/com.pa.mrh0.not-enough-energy/icon_atlas.js
+++ ui/mods/com.pa.mrh0.not-enough-energy/icon_atlas.js
@@ -1,5 +1,6 @@
 var icons = [
     "titan_energy_plant",
+	"l_titan_energy_plant",
     "energy_plant_improved"
 ];
 model.strategicIcons(model.strategicIcons().concat(icons));
--- ui/mods/com.pa.mrh0.not-enough-energy/shared_build.js
+++ ui/mods/com.pa.mrh0.not-enough-energy/shared_build.js
@@ -1,5 +1,6 @@
 var newBuild = {
     "/pa/units/land/titan_energy_plant/titan_energy_plant.json": ["utility", 0, { row: 0, column: 7, titans: true }],
+	"/pa/units/land/l_titan_energy_plant/l_titan_energy_plant.json": ["L_utility", 0, { row: 1, column: 6, titans: true }],
     "/pa/units/land/energy_plant_adv/energy_plant_adv.json": ["utility", 0, { row: 1, column: 7, titans: true }],
     "/pa/units/land/energy_plant_improved/energy_plant_improved.json": ["utility", 0, { row: 0, column: 3, titans: true }],
 }
